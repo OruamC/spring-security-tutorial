e73bf75bcc96c180b4dde6bfc609bd84ccc59a87
Oruam <oru_catani@hotmail.com>
2021-04-14 14:16:18 -0300

    Using permissions based authentication

--- src/main/java/com/springsecurity/tutorial/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecurity/tutorial/security/ApplicationSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.springsecurity.tutorial.security;
 
-import com.springsecurity.tutorial.student.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,8 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.springsecurity.tutorial.security.ApplicationUserRole.ADMIN;
-import static com.springsecurity.tutorial.security.ApplicationUserRole.STUDENT;
+import static com.springsecurity.tutorial.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.springsecurity.tutorial.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -30,9 +30,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // TODO: Resolver
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE ,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST ,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT ,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET ,"management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,18 +50,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
